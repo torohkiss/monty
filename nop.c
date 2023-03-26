1090aa58bd8e5a7f9e22b0fdb2e247d991ef8b71
torohkiss <torohkiss@yahoo.com>
2023-03-26 15:40:28 -0700

    yasks

---
+++ nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * nop -  does nothing
+ * @stack: doesnt matter
+ * @line_cnt : for nothing
+ *    
+ * Return: 0
+ */
+
+
+void nop(stack_t **stack, unsigned int line_cnt)
+{
+	(void) stack;
+	(void) line_cnt;
+}
